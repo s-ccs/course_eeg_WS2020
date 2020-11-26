2465ed652c0849bab78c3773fed2e3795cfb0913
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2020-11-26 15:20:58 +0100

    bugfixes

--- exercises/ccs_eeg_semesterproject.py
+++ exercises/ccs_eeg_semesterproject.py
@@ -2,6 +2,7 @@
 import os
 import mne
 import numpy as np
+import pandas as pd
 from mne_bids import (BIDSPath, read_raw_bids)
 
 def _get_filepath(bids_root,subject_id):
@@ -14,18 +15,29 @@ def _get_filepath(bids_root,subject_id):
 
 def load_precomputed_ica(bids_root,subject_id):
     fn = _get_filepath(bids_root,subject_id)+'ica'
+    #bids_root 
 
     # import the eeglab ICA. I used eeglab because the "amica" ICA is a bit more powerful than runica
     ica = mne.preprocessing.read_ica_eeglab(fn+'.set')
-    
+    #ica = custom_read_eeglab_ica(fn+'.set')
     # Potentially for plotting one might want to copy over the raw.info, but in this function we dont have access / dont want to load it
     #ica.info = raw.info
     ica._update_ica_names()
     badComps = np.loadtxt(fn+'.tsv',delimiter="\t")
+    badComps -= 1 # start counting at 0
     return ica,badComps
 
 def load_precomputed_badData(bids_root,subject_id):
     fn = _get_filepath(bids_root,subject_id)
-    annotations = mne.read_annotations(fn+'badSegments.csv')
+    
+    tmp = pd.read_csv(fn+'badSegments.csv')
+    #print(tmp)
+    annotations = mne.Annotations(tmp.onset,tmp.duration,tmp.description)
+    # Unfortunately MNE assumes that csv files are in milliseconds and only txt files in seconds.. wth?
+    #annotations = mne.read_annotations(fn+'badSegments.csv')
     badChannels = np.loadtxt(fn+'badChannels.tsv',delimiter='\t')
+    badChannels = badChannels.astype(int)
+    #badChannels = [int(b) for b in badChannels]
     return annotations,badChannels
+
+
